e7a243f1a7d64ee75d2223870ef194f399ed2056
azernov <azernov@incom-sr.ru>
2024-11-08 16:11:54 +0300

    9. Сервисы: основа всего

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -6,12 +6,15 @@ use App\Model\Starship;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Psr\Log\LoggerInterface;
 
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(LoggerInterface $logger): Response
     {
+        $logger->info('Starship collection retrieved');
+        
         $starships = [
             new Starship(
                 1,
